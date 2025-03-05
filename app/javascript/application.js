961fed71b60f52ea520664e995da485182ce71c0
Tatsuto8888 <longdouzhongcun500@gmail.com>
2025-03-05 18:20:12 +0900

    add:ハンバーガーメニュー実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// ハンバーガーメニューに表示・非表示の動きをつける
+document.addEventListener("turbo:load", function() {
+    const menu = document.getElementById("menu");
+    const button = document.getElementById("hamburger-button");
+  
+    button.addEventListener("click", function() {
+      menu.classList.toggle("hidden"); // hidden クラスを切り替え
+    });
+});
