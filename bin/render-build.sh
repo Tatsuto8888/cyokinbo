7b1afae5924ea53c79e79aaa5a226f0f1a4e1472
Tatsuto8888 <longdouzhongcun500@gmail.com>
2025-03-13 02:53:28 +0900

    render-build.shの内容変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,6 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
-
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# 本番環境のデータを一度リセットしてくれるコマンド（下記コマンド）
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:migrate
