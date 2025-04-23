4074cc038a9bea2a51ed55c4cfb9feed6fdaf4bb
Tatsuto8888 <longdouzhongcun500@gmail.com>
2025-04-24 06:25:54 +0900

    add:コメント変数追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,15 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [ :index ]
-  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :set_post, only: [ :edit, :update, :destroy ]
 
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user).order(created_at: :desc)
   end
 
   def new
